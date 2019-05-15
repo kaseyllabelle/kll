6823c1670cd2074bdabf5ff6361e1e1b294ffcac
Kasey Labelle <kasey.l.labelle@gmail.com>
2019-05-14 23:22:24 -0400

    add dynamic copyright date, add contact form, update icon styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,26 +8,29 @@ export default function Footer(props)
 				<div className="col col-12">
 					<div className="footer-links">
 						<a href="mailto:kll.design.dev@gmail.com" className="email">
-							<i className="fas fa-envelope-square fa-lg social-link-icon" title="Email"></i>
+							<i className="fas fa-envelope fa-fw fa-lg footer-link-icon" title="Email"></i>
+						</a>
+						<a href="https://goo.gl/forms/fcvTryc6Pryrxjfj2" className="google-form">
+							<i className="fab fa-google fa-fw fa-lg footer-link-icon" title="Form"></i>
 						</a>
 						<a href="https://www.linkedin.com/in/kaseyllabelle" className="linkedin">
-							<i className="fab fa-linkedin fa-lg footer-link-icon" title="LinkedIn"></i>
+							<i className="fab fa-linkedin-in fa-fw fa-lg footer-link-icon" title="LinkedIn"></i>
 						</a>
 						<a href="https://github.com/kaseyllabelle" className="github">
-							<i className="fab fa-github-square fa-lg footer-link-icon" title="GitHub"></i>
+							<i className="fab fa-github fa-fw fa-lg footer-link-icon" title="GitHub"></i>
 						</a>
 						<a href="https://twitter.com/kaseyllabelle" className="twitter">
-							<i className="fab fa-twitter-square fa-lg footer-link-icon" title="Twitter"></i>
+							<i className="fab fa-twitter fa-fw fa-lg footer-link-icon" title="Twitter"></i>
 						</a>
 						<a href="http://thatsthefont.tumblr.com/" className="tumblr">
-							<i className="fab fa-tumblr-square fa-lg footer-link-icon" title="Tumblr"></i>
+							<i className="fab fa-tumblr fa-fw fa-lg footer-link-icon" title="Tumblr"></i>
 						</a>
 					</div>
 				</div>
 			</div>
 			<div className="row">
 				<div className="col col-12">
-					<p className="copyright"><i className="far fa-copyright fa-sm" title="copyright"></i> 2018 Kasey L. Labelle</p>
+					<p className="copyright"><i className="far fa-copyright fa-sm" title="copyright"></i> {(new Date().getFullYear())} Kasey L. Labelle</p>
 				</div>
 			</div>
 		</footer>
--- src/containers/info/info.contact.js
+++ src/containers/info/info.contact.js
@@ -8,31 +8,37 @@ export default function Contact(props)
 			<div className="social-links">
 				<div className="social-link email">
 					<a href="mailto:kll.design.dev@gmail.com" className="social-link-text">
-						<i className="fas fa-envelope-square fa-lg social-link-icon" title="Email"></i>
+						<i className="fas fa-envelope fa-fw fa-lg social-link-icon" title="Email"></i>
 						kll.design.dev
+					</a>
+				</div>
+				<div className="social-link google-form">
+					<a href="https://goo.gl/forms/fcvTryc6Pryrxjfj2" className="social-link-text">
+						<i className="fab fa-google fa-fw fa-lg social-link-icon" title="Form"></i>
+						contact form
 					</a>
 				</div>
 				<div className="social-link linkedin">
 					<a href="https://www.linkedin.com/in/kaseyllabelle" className="social-link-text">
-						<i className="fab fa-linkedin fa-lg social-link-icon" title="LinkedIn"></i>
+						<i className="fab fa-linkedin-in fa-fw fa-lg social-link-icon" title="LinkedIn"></i>
 						linkedin.com/in/kaseyllabelle
 					</a>
 				</div>
 				<div className="social-link github">
 					<a href="https://github.com/kaseyllabelle" className="social-link-text">
-						<i className="fab fa-github-square fa-lg social-link-icon" title="GitHub"></i>
+						<i className="fab fa-github fa-fw fa-lg social-link-icon" title="GitHub"></i>
 						github.com/kaseyllabelle
 					</a>
 				</div>
 				<div className="social-link twitter">
 					<a href="https://twitter.com/kaseyllabelle" className="social-link-text">
-						<i className="fab fa-twitter-square fa-lg social-link-icon" title="Twitter"></i>
+						<i className="fab fa-twitter fa-fw fa-lg social-link-icon" title="Twitter"></i>
 						twitter.com/kaseyllabelle
 					</a>
 				</div>
 				<div className="social-link tumblr">
 					<a href="http://thatsthefont.tumblr.com/" className="social-link-text">
-						<i className="fab fa-tumblr-square fa-lg social-link-icon" title="Tumblr"></i>
+						<i className="fab fa-tumblr fa-fw fa-lg social-link-icon" title="Tumblr"></i>
 						thatsthefont.tumblr.com
 					</a>
 				</div>
