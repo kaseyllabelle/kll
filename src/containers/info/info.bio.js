d0a532a3cf58cb68da951691d7e10b4d886f6ba3
Kasey Labelle <kasey.l.labelle@gmail.com>
2019-09-02 14:22:28 -0400

    fixing white space

--- src/containers/info/info.bio.js
+++ src/containers/info/info.bio.js
@@ -10,7 +10,7 @@ export default function Bio(props)
 				I'm a Graphic and Web Designer and Developer living and working in the Greater Boston Area. I am passionate about front-end development, design, art, diversity, accessibility, and&nbsp;community.
 			</p>
 			<p className="paragraph">
-				I joined Epsilon as a Senior UX Front End Developer in October 2018. Previously, I was a Senior Software Engineer at Vistaprint, where I also held leadership roles in the Pride and Women in Technology employee resource groups and actively participated in the Inclusion &nbsp;group.
+				I joined Epsilon as a Senior UX Front End Developer in October 2018. Previously, I was a Senior Software Engineer at Vistaprint, where I also held leadership roles in the Pride and Women in Technology employee resource groups and actively participated in the Inclusion&nbsp;group.
 			</p>
 			<p className="paragraph">
 				I’m proud to serve as a City Director for Lesbians Who Tech Boston, and moderated the group’s 2017 Out Leadership Panel. I’ve been on the other side of the panel experience, too. I was a panelist on the Lesbians Who Tech Roundtable at Wentworth Institute of Technology in November 2018, the Step up and Be a Leader panel at ACT-W Boston in August 2017, and the Women in Careers Meet and Greet panel at Grafton High School in January&nbsp;2017.
