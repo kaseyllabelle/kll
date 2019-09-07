36e41bbbad17e0907013aad42311f888cc8805e6
Kasey Labelle <kasey.l.labelle@gmail.com>
2019-09-07 12:04:18 -0400

    filter styling

--- src/components/home.filters.js
+++ src/components/home.filters.js
@@ -5,7 +5,7 @@ export default function Filters(props)
   const filtersArray = ['All', 'Design', 'Development', 'Fine Art'];
   const filters = filtersArray.map((item) => {
     return(
-      <button key={btoa (item)} onClick={(e) => {props.filterProjectsProp(item)}} data-filter={item} className="filter">
+      <button key={btoa (item)} onClick={(e) => {props.filterProjectsProp(item)}} data-filter={item} className="filter" aria-label={`Filter projects by ${item}`}>
         {item}
       </button>
     )
