6683c025571d1f99e2cdb145e3ffd3ca1c55b950
Kasey Labelle <kasey.l.labelle@gmail.com>
2020-01-24 12:36:17 -0500

    updated bio

--- src/containers/info/info.bio.js
+++ src/containers/info/info.bio.js
@@ -10,16 +10,16 @@ export default function Bio(props)
         I'm a Graphic and Web Designer and Developer living and working in the Greater Boston Area. I am passionate about front-end development, design, art, diversity, accessibility, and&nbsp;community.
       </p>
       <p className="paragraph">
-        I joined Epsilon as a Senior UX Front End Developer in October 2018. Previously, I was a Senior Software Engineer at Vistaprint, where I also held leadership roles in the Pride and Women in Technology employee resource groups and actively participated in the Inclusion&nbsp;group.
+        I joined Epsilon as a Senior UX Front End Developer in October 2018. Previously, I was a Senior Software Engineer at Vistaprint, where I held leadership roles in the Pride and Women in Technology employee resource groups and was an integral participant in the Inclusion&nbsp;group.
       </p>
       <p className="paragraph">
-        I’m proud to serve as a City Director for Lesbians Who Tech Boston, and moderated the group’s 2017 Out Leadership Panel. I’ve been on the other side of the panel experience, too. I was a panelist on the Lesbians Who Tech Roundtable at Wentworth Institute of Technology in November 2018, the Step up and Be a Leader panel at ACT-W Boston in August 2017, and the Women in Careers Meet and Greet panel at Grafton High School in January&nbsp;2017.
+        I’m proud to serve as a City Director for Lesbians Who Tech Boston, and to have moderated the group’s 2017 Out Leadership Panel. I’ve been on the other side of the panel experience, too. I was a panelist on the Lesbians Who Tech Roundtable at Wentworth Institute of Technology in November 2018, the Step up and Be a Leader panel at ACT-W Boston in August 2017, and the Women in Careers Meet and Greet panel at Grafton High School in January&nbsp;2017.
       </p>
       <p className="paragraph">
-        I have an insatiable appetite for knowledge. I received my B.A. in Studio Arts with a concentration in Graphic Design from Salve Regina University in 2010, and I've completed several continuing ed courses since. Most recently, I graduated from Thinkful's Full Stack Flex program and completed ThinkB!G.LearnSmart's course on Web Accessibility, WCAG 2.0, and Section 508&nbsp;Compliance.
+        I received my B.A. in Studio Arts with a concentration in Graphic Design from Salve Regina University in 2010, and I've since completed multiple continuing ed courses. Most recently, I graduated from Thinkful's Full Stack Flex program and completed ThinkB!G.LearnSmart's course on Web Accessibility, WCAG 2.0, and Section 508&nbsp;Compliance.
       </p>
       <p className="paragraph">
-        I love puppies, sunshine, weird art, and puns. I have a knack for finding typos. I finally figured out how to keep plants alive, and I’m super excited about my green(-ish) thumb. I know how to fold a fitted&nbsp;sheet.
+        I love puppies, sunshine, weird art, and puns. I have a knack for finding typos, I know how to fold a fitted sheet, and I finally figured out how to keep plants&nbsp;alive.
       </p>
       <p className="paragraph">
         Friends and colleagues describe me as: passionate, vibrant, a force, a fearless leader, a unicorn, and the most organized person [they've] ever&nbsp;met.
